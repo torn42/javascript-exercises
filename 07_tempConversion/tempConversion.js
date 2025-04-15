21c5b3c2bda8acbe8b175039e9d7f8914adef50a
torn <zhabkapepe5@gmail.com>
2025-04-15 18:34:52 +0500

    solved javascript exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  const result = (temperature - 32) * (5 / 9);
+  return Number.isInteger(result) ? result : Number(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  const result = temperature * (9 / 5) + 32;
+  return Number.isInteger(result) ? result : Number(result.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
